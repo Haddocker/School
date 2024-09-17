82a0573451bb18db2c8fa49341bdda3c313d75b8
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-17 14:05:21 +0200

    Add files via upload

---
+++ Modul 5/Assignment 04/enter_password/enter_password/Program.cs
@@ -0,0 +1,19 @@
+﻿int guessCounter = 0;
+
+while (guessCounter < 3)
+{
+    Console.Write("Write the password here: ");
+    string userGuess = Console.ReadLine();
+
+    if (userGuess == "password")
+    {
+        Console.WriteLine("That is correct!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("That is incorrect, try again...");
+        guessCounter++;
+    }
+
+}
---
+++ Modul 5/Assignment 05/random_number/random_number/Program.cs
@@ -0,0 +1,15 @@
+﻿Random random = new Random();
+
+int randomNumber = random.Next(1, 10);
+// Console.WriteLine($"pssst the correct number is: {randomNumber}");
+
+Console.Write("See if you can guess the random number (between 1 and 10): ");
+int userGuess;
+
+do
+{
+    Console.Write("That was wrong try again: ");
+    userGuess = Convert.ToInt32(Console.ReadLine());
+} while (userGuess != randomNumber);
+
+Console.WriteLine("That's correct!");
