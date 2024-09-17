43925b13a19adb45a574235e5d7a22691c1f9615
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-17 12:46:18 +0200

    Add files via upload

---
+++ Modul 3/Assignment 10/nice_price/nice_price/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Type a price with one decimal: ");
+double price = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"The price is: {price:N2");
---
+++ Modul 3/Find 5 mistakes about variables/research/research/Program.cs
@@ -0,0 +1,14 @@
+﻿// Programmet her skal indlæse brugerens navn og alder,
+// og fortælle, hvor gammel vedkommende bliver til næste år.
+using System;
+
+Console.WriteLine("Hvad hedder du?");
+string navn = Console.ReadLine();
+
+Console.WriteLine("Hvor gammel er du?");
+int alder = Convert.ToInt32(Console.ReadLine());
+
+int nyAlder = alder + 1;
+
+Console.WriteLine($"Hej {navn}");
+Console.WriteLine($"Til næste år bliver du {nyAlder} år");
