dbd71e9ecd08a04ee3fdb4f70f9ab7045731cb63
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-17 12:48:23 +0200

    Add files via upload

---
+++ Modul 4/Assignment 01/name_checker/name_checker/Program.cs
@@ -0,0 +1,13 @@
+﻿const string good_name = "alexander";
+
+Console.Write("What is your name?: ");
+string user_name = Console.ReadLine().ToLower();
+
+if (user_name == good_name)
+{
+    Console.WriteLine("Wow that is a beautiful name!");
+}
+else
+{
+    Console.WriteLine("That's an okay name...");
+}
