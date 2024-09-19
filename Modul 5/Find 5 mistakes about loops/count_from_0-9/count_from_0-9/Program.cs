18ee6c3e161ad6d0b2d1af5a315d788887dfcf7c
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-19 13:48:28 +0200

    Add files via upload

---
+++ Modul 5/Find 5 mistakes about loops/count_from_0-9/count_from_0-9/Program.cs
@@ -0,0 +1,17 @@
+﻿// Programmet skal udskrive tallene fra 0 til 9
+// med den detalje at i stedet for tallet 5 skal teksten FEM udskrives
+// (så det bliver 0 1 2 3 4 FEM 6 7 8 9)
+
+for (int i = 0; i < 10; i++)
+{
+    if (i == 5)
+    {
+        Console.Write("FEM");
+        Console.Write(" ");
+    }
+    else
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+}
