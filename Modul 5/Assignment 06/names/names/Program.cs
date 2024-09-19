78950a78ef3e115f4e9b53b3c31267fa5de7803a
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-19 09:53:42 +0200

    Add files via upload

---
+++ Modul 5/Assignment 06/names/names/Program.cs
@@ -0,0 +1,17 @@
+﻿List<string> participantNames = [];
+string names;
+
+do
+{
+    Console.Write("Write the names of the participant (if you're done press 'enter' key): ");
+    names = Console.ReadLine();
+
+    // Stops the script from adding "Enter Key - Blank Space" as a participant. 
+    if (!string.IsNullOrEmpty(names))
+    {
+        participantNames.Add(names);
+    }
+
+} while (!string.IsNullOrEmpty(names));
+
+Console.WriteLine("Participants: " + string.Join(", ", participantNames));
