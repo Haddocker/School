655ebfaf83dcde89de94ebd3942af0e3a36f96a4
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-19 12:24:59 +0200

    Add files via upload

---
+++ Modul 5/Assignment 07/99_bottles_of_beer/99_bottles_of_beer/Program.cs
@@ -0,0 +1,25 @@
+﻿for (int currentBottles = 99; currentBottles > -2; currentBottles--)
+{
+    int nextBottles = currentBottles - 1;
+
+    if (currentBottles >= 1)
+    {
+        Console.WriteLine($"{currentBottles} bottles of beer on the wall, {currentBottles} bottles of beer.\n" +
+        $"Take one down and pass it around, {nextBottles} bottles of beer on the wall.\n" +
+        "");
+    }
+
+    else if (currentBottles == 0)
+    {
+        Console.WriteLine($"{currentBottles} bottles of beer on the wall, {currentBottles} bottles of beer.\n" +
+        "Take one down and pass it around, no more bottles of beer on the wall.\n" +
+        "");
+    }
+
+    else
+    {
+        Console.WriteLine("No more bottles of beer on the wall, no more bottles of beer.\n" +
+            "Go to the store and buy some more, 99 bottles of beer on the wall.\n" +
+            "");
+    }
+}
