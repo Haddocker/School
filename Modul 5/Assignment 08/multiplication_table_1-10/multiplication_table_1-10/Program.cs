ad3294280fd5dc0f5705578fe5efe365fe3552f8
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-19 13:32:35 +0200

    Add files via upload

---
+++ Modul 5/Assignment 08/multiplication_table_1-10/multiplication_table_1-10/Program.cs
@@ -0,0 +1,9 @@
+﻿
+for (int line = 1; line < 11; line++)
+{
+    for (int number = line; number < line * 10 + 1; number = number + line)
+    {
+        Console.Write($" {number,3} |");
+    }
+    Console.WriteLine("\n------------------------------------------------------------");
+}
---
+++ Modul 5/Assignment 09/multiplication_table_userinput/multiplication_table_userinput/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("What multiplication table do you want to see (1-10): ");
+int table = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+for  (int number = 1; number < 11; number++)
+{
+    Console.WriteLine(number * table);
+}
