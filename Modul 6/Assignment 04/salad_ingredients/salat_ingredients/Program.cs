6a718b6c1989f629f9b2e667f2c0483b8c8b5678
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-25 11:37:45 +0200

    Add files via upload

---
+++ Modul 6/Assignment 04/salad_ingredients/salat_ingredients/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Write 3 ingredients you want in your salad (press enter after each one): ");
+string[] saladIngredients = new string[3];
+
+for (int i = 0; i < saladIngredients.Length; i++)
+{
+    saladIngredients[i] = Console.ReadLine();
+}
+
+Console.WriteLine("");
+for  (int i = 2; i >= 0; i--)
+{
+    Console.WriteLine(saladIngredients[i]);
+}
