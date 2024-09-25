e0dada192608859c4fb431025dbc4579780e7d32
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-25 11:25:46 +0200

    Add files via upload

---
+++ Modul 6/Assignment 03/favourite_things/favourite_things/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] favouriteThings = new string[3];
+
+for (int i = 0; i < favouriteThings.Length; i++)
+{
+    Console.Write("Type in one of your favourite things: ");
+    favouriteThings[i] = Console.ReadLine();
+}
+
+
+Console.WriteLine($"Your favourite things are:\n" +
+    $"1: {favouriteThings[0]}\n" +
+    $"2: {favouriteThings[1]}\n" +
+    $"3: {favouriteThings[2]}");
