94f8decbe2f68e092a0848fa1d2bc5aa2aca7695
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-25 10:40:49 +0200

    Add files via upload

---
+++ Modul 6/Assignment 01/different_sandwiches/different_sandwiches/Program.cs
@@ -0,0 +1,7 @@
+﻿
+string[] sandwiches = new string[] {"Ham and cheese", "BLT", "Meatballs"};
+
+foreach (string sandwich in sandwiches)
+{
+    Console.WriteLine(sandwich);
+}
---
+++ Modul 6/Assignment 02/winning_numbers/winning_numbers/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] numbers = [2, 9, 15, 23, 26, 35, 36];
+
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
