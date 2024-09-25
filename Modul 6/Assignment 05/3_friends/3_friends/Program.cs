097aaed4dd9ba961ea7a298ed528d946931174ca
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-25 14:54:57 +0200

    Add files via upload

---
+++ Modul 6/Assignment 05/3_friends/3_friends/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Write the names of three of your friends (press enter after each one): ");
+string[] names = new string[3];
+
+for (int i = 0; i < names.Length; i++)
+{
+    names[i] = Console.ReadLine();
+}
+
+// bool containsGoodName = names.Contains("alexander");
+// Console.WriteLine("The array contains a good name: " +  containsGoodName);
+
+
+foreach (string name in names)
+{
+    if (name == "alexander")
+    {
+        Console.WriteLine("Nice that you remembered me!");
+    }
+}
