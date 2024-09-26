34bd077bb3ec446f8e1819978d2d95991e440da6
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-26 13:49:57 +0200

    Add files via upload

---
+++ Modul 6/Assignment 08/to_do_list/to_do_list/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("How many things do you have to remember?: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string[] toDoList = new string[number];
+
+for (int i = 0; i < number; i++)
+{
+    Console.WriteLine($"Enter the thing on your todo list number {i + 1}: ");
+    toDoList[i] = Console.ReadLine();
+}
+
+Console.WriteLine("");
+for (int i = 0; i < number; i++)
+{
+    Console.WriteLine($"{i + 1}: {toDoList[i]}");
+}
