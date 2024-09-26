b86e518eaa60819616fadf5da69c68ee7cb955eb
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-26 13:07:25 +0200

    Add files via upload

---
+++ Modul 6/Assignment 06/prices/prices/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Enter the name of the three goods and their prices: ");
+string[] goods = new string[3];
+double[] prices = new double[3];
+
+for (int i = 0; i < goods.Length; i++)
+{
+    Console.Write($"Enter the name of good {i + 1}: ");
+    goods[i] = Console.ReadLine();
+
+    Console.Write($"Enter the price of {goods[i]}: ");
+    prices[i] = Convert.ToDouble(Console.ReadLine());
+}
+
+Console.WriteLine("");
+for  (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"{goods[i]} costs {prices[i]} kr.");
+}
