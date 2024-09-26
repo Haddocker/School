5d8e39a44bc229b581c4a691aff53eb1bfdfbe33
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-09-26 13:31:13 +0200

    Add files via upload

---
+++ Modul 6/Assignment 07/bigger_than_10/bigger_than_10/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Enter 5 different numbers: ");
+int[] numbers = new int[5];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 10)
+    {
+        Console.WriteLine($"{numbers[i]} is bigger than 10");
+    }
+}
