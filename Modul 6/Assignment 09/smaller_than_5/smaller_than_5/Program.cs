91d2ad638c7749838d6737b6e2cf836cfe0b0bee
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-10-07 18:07:20 +0200

    Add files via upload

---
+++ Modul 6/Assignment 09/smaller_than_5/smaller_than_5/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Enter 5 numbers: ");
+double[] numbers = new double[5];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = Convert.ToDouble(Console.ReadLine());
+}
+
+
+Console.WriteLine("");
+
+
+int smallerThan5 = 0;
+foreach (double j in numbers)
+{
+    if (j < 5)
+    {
+        smallerThan5++;
+    }
+}
+
+Console.WriteLine($"{smallerThan5} of your numbers are smaller than 5.");
