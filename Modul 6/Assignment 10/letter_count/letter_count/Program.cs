08a4e5f0392b0d24efab42a31af44d6860a8f6a2
Haddocker <93126047+Haddocker@users.noreply.github.com>
2024-10-07 18:20:28 +0200

    Add files via upload

---
+++ Modul 6/Assignment 10/letter_count/letter_count/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter 5 words: ");
+string[] words = new string[5];
+
+for (int i = 0; i < words.Length; i++)
+{
+    words[i] = Console.ReadLine();
+}
+
+Console.WriteLine("");
+
+foreach (string word in words)
+{
+    if (word.Length == 4)
+    {
+        Console.WriteLine(word);
+    }
+}
